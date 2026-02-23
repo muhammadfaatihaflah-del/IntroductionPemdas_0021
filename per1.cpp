2ba4c56b84b5dd8126ef505cfb5e74b4ec3064cf
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-02-23 11:15:49 +0700

    membuat program main

--- per1.cpp
+++ per1.cpp
@@ -1,4 +1,12 @@
-#include <iostream>
+#include <iostream> //library
 //using namespace std;
 using std::cout;
 using std::endl;
+
+int main() // main program
+{
+    //menampilkan string
+    cout << "selamat datang di TI UMY" << endl;
+    //menampilkan numeris
+    cout << 10;
+}
